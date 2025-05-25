f0784a714f8a83b81e120e32df31d2b2c315f415
3roaa <roaaqarnia@gmail.com>
2025-05-25 19:59:29 +0300

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-
+// في ملف script.js
+document.addEventListener('DOMContentLoaded', function() {
+    // تحديث سنة حقوق النشر تلقائيًا
+    document.getElementById('year').textContent = new Date().getFullYear();
+});
